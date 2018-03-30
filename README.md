16168b954b31b58b296656a6284968fb5c229dd7
Kuty <quentin.le-baron@epitech.eu>
2018-03-30 06:28:20 +0200

    Title: Cron each 30 seconds, add dict library. Ref: no ref Author: kuty Feature: cron-function Description:
    	cron each 30  to prepare insert dB function.

--- README.md
+++ README.md
@@ -4,5 +4,90 @@ Usefull scroller for Bittrex platform using websocket with lots of use cases.
 To be able to start the scroller you need to do:
 
 ```
+npm install dict --save
+npm install --save node-cron
 npm install
 ```
+
+# Scroller Ticker Bot
+
+### summary:
+
+## Introduction
+
+
+### Requirements:
+- [Docker](https://www.docker.com)
+- [Docker-compose](https://docs.docker.com/compose/)
+or
+- [Ansible]()
+## Install
+
+## Start
+
+
+## logs
+you can verify your module is up with:
+```
+  make ps
+```
+
+for logs:
+```
+  # all logs
+  make logs
+```
+
+## Documentation
+
+_Makefile commands available_:
+
+| **commands name** | **description**        |
+|:-----------------:|:---------------------- |
+|      `make`       | 1. down each service   |
+|                   | 2. build basic project |
+|                   | 3. run project         |
+|                   |                        |
+|  `make build_up`  | 1. build basic project |
+|                   | 2. run project         |
+|                   |                        |
+|   `make build`    | build basic project.   |
+|                   |                        |
+|     `make up`     | run project            |
+|                   |                        |
+|    `make down`    | down project           |
+|                   |                        |
+|     `make ps`     | list container         |
+|                   |                        |
+|    `make logs`    | display all logs       |
+
+_Tricks_:
+
+|              **commands**              | **description**         |
+|:--------------------------------------:|:----------------------- |
+| `docker network create "Network-Name"` | Create a docker network |
+|          `docker network ls`           | List docker network     |
+
+_Troubles possible_:
+
+make logs failed:
+if the commands failed use the docker-compose command:
+`docker-compose logs`
+
+## Reference
+
+
+> - [Mysql compose hub](https://hub.docker.com/_/mysql/)
+> - [Mysql documentation](https://dev.mysql.com/doc/)
+> - [Docker website](https://www.docker.com)
+> - [Docker-compose documentation](https://docs.docker.com/compose/)
+> - [Git documentation](https://git-scm.com/documentation)
+> - [Docker network documentation](https://docs.docker.com/engine/userguide/networking/work-with-networks/)
+
+
+## version
+
+| **Version** | **Description**                                       |
+|:-----------:|:----------------------------------------------------- |
+|    0.0.1    | Connect websocket client and display ticker (handler) |
+|             |                                                       |
--- bittrex_listener.js
+++ bittrex_listener.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql');
+var dict = require("dict");
+var cron = require('node-cron');
 const bittrex = require('./node.bittrex.api');
 
 var multiplier = 100000000;
@@ -10,10 +12,41 @@ var con = mysql.createConnection({
   password: "lmao22",
   database: "Askip_toolTradingBox"
 });
+var d = dict({
+    MarketName: 0,
+    Bid: 0,
+    Ask: 0,
+    Last: 0,
+    High: 0,
+    Low: 0,
+    Volume: 0,
+    BaseVolume: 0,
+    OpenBuyOrders: 0,
+    OpenSellOrders: 0,
+    PrevDay: 0
+});
+var MarketList = new Array();
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+});
+
+var insertToDB = function(marketsDelta){
+  var sql = "INSERT INTO ticker(id_bot, marketName, bid,ask,last, high, low, \
+    volume, base_volume, open_buy_orders, open_sell_orders, moy_prev_day) \
+    VALUES(" + "4242" + ", \'" + marketsDelta.MarketName + "\', \'" + marketsDelta.Bid * multiplier + "\',\
+    \'" + marketsDelta.Ask * multiplier + "\', \'" + marketsDelta.Last * multiplier + "\', \'" + marketsDelta.High * multiplier + "\',\
+    \'" + marketsDelta.Low * multiplier + "\', \'" + marketsDelta.Volume + "\', \'" + marketsDelta.BaseVolume + "\',\
+    \'" + marketsDelta.OpenBuyOrders + "\', \'" + marketsDelta.OpenSellOrders + "\', \'" + marketsDelta.PrevDay * multiplier + "\')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    //console.log("Result: " + result);
+  });
+};
+
+cron.schedule('0,30 * * * * *', function(){
+  console.log('running a task every 30 seconds');
 });
 
 bittrex.options({
@@ -25,17 +58,17 @@ bittrex.options({
         if (data.M === 'updateSummaryState') {
           data.A.forEach(function(data_for) {
             data_for.Deltas.forEach(function(marketsDelta) {
-              //console.log('Ticker Update for '+ marketsDelta.MarketName, marketsDelta);
-              var sql = "INSERT INTO ticker(id_bot, marketName, bid,ask,last, high, low, \
-                volume, base_volume, open_buy_orders, open_sell_orders, moy_prev_day) \
-                VALUES(" + "4242" + ", \'" + marketsDelta.MarketName + "\', \'" + marketsDelta.Bid * multiplier + "\',\
-                \'" + marketsDelta.Ask * multiplier + "\', \'" + marketsDelta.Last * multiplier + "\', \'" + marketsDelta.High * multiplier + "\',\
-                \'" + marketsDelta.Low * multiplier + "\', \'" + marketsDelta.Volume + "\', \'" + marketsDelta.BaseVolume + "\',\
-                \'" + marketsDelta.OpenBuyOrders + "\', \'" + marketsDelta.OpenSellOrders + "\', \'" + marketsDelta.PrevDay * multiplier + "\')";
-              con.query(sql, function (err, result) {
-                if (err) throw err;
-                //console.log("Result: " + result);
-              });
+              // console.log('Ticker Update for '+ marketsDelta.MarketName);//, marketsDelta);
+              // var sql = "INSERT INTO ticker(id_bot, marketName, bid,ask,last, high, low, \
+              //   volume, base_volume, open_buy_orders, open_sell_orders, moy_prev_day) \
+              //   VALUES(" + "4242" + ", \'" + marketsDelta.MarketName + "\', \'" + marketsDelta.Bid * multiplier + "\',\
+              //   \'" + marketsDelta.Ask * multiplier + "\', \'" + marketsDelta.Last * multiplier + "\', \'" + marketsDelta.High * multiplier + "\',\
+              //   \'" + marketsDelta.Low * multiplier + "\', \'" + marketsDelta.Volume + "\', \'" + marketsDelta.BaseVolume + "\',\
+              //   \'" + marketsDelta.OpenBuyOrders + "\', \'" + marketsDelta.OpenSellOrders + "\', \'" + marketsDelta.PrevDay * multiplier + "\')";
+              // con.query(sql, function (err, result) {
+              //   if (err) throw err;
+              //   //console.log("Result: " + result);
+              // });
             });
           });
         }
